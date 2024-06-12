3359ac006596cbaf29845c45fa9d48d4657a0840
anaserrami <anaserrami24@gmail.com>
2024-06-12 17:22:11 +0100

    add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import AdminDashboard from './components/AdminDashboard';
+import UserDashboard from './components/UserDashboard';
+import './App.css';
+
+function App() {
+  return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/user" element={<UserDashboard />} />
+        </Routes>
+      </Router>
+  );
+}
+
+export default App;
